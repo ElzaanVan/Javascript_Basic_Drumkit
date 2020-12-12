af8242e551af779480da00374db73dedd02b0ed8
ElzaanVan <75164867+ElzaanVan@users.noreply.github.com>
2020-12-12 22:08:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ document.addEventListener("keydown", function(event){
 function makeSound(key){
     switch (key) {
     case "w":
-    var tom1 = new Audio("sounds/tom-1.mp3");
+    var tom1 = new Audio("/sounds/tom-1.mp3");
     tom1.play();
     break;
     case "a" :
-    var tom2 = new Audio("sounds/tom-2.mp3");
+    var tom2 = new Audio("/sounds/tom-2.mp3");
     tom2.play();
     break;
     case "s" :
-    var tom3 = new Audio("sounds/tom-3.mp3");
+    var tom3 = new Audio("/sounds/tom-3.mp3");
     tom3.play();
     break;
     case "d" :
